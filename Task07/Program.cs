775d2c7854c5a24d39b5b2844eff7d87ecde6b94
«Sergeityumen72» <>
2023-08-17 19:02:59 +0500

    D/Z

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -7,5 +7,10 @@
 
 Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
+if(number <0) number *= -1;
+if(number >= 100 && number <= 999)
+{
 int lastDigit = number % 10;
 Console.WriteLine($"Посленяя цифра {lastDigit}");
+}
+else Console.WriteLine("Введено некоректное число");

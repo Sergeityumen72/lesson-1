a85a6b409d3cc5cd0454629cefc87d450e9ca524
«Sergeityumen72» <>
2023-08-25 17:06:52 +0500

    dz

---
+++ Task09/Program.cs
@@ -0,0 +1,16 @@
+﻿// int number = new Random().Next(10, 100);  // 99+1
+// Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {number}");
+
+// int firstDigit = namber / 10;
+// int secondDigit = namber % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = nam / 10;
+    int secondDigit = nam % 10;
+    if (firstDigit > secondDigit) return firstDigit;
+    return secondDigit;
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное• число из диапазона• 100-999 • -› - (number)");
+
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int result = firstDigit * 10 + lastDigit;
+Console.WriteLine(result);
+
+int twoNambers = TwoNumbers(number);
+Console.WriteLine(twoNambers);
+
+int TwoNumbers(int num)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int result = firstDigit * 10 + lastDigit;
+}

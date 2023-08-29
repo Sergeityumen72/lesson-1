679ba32c3bf19da38c1334fce9b7fa861748de77
«Sergeityumen72» <>
2023-08-29 15:39:19 +0500

    dz

---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// 14 Напишите программу которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23.
+// 14 -> Нет
+// 26 -> Нет
+// 161 -> Да
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultiplicityTwoDigit(num, 7, 23);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool MultiplicityTwoDigit(int numb, int numb1, int numb2)
+{
+    return (numb % numb1 == 0 && numb % numb2 == 0);
+
+}

e413962d1cc6fd691058444cb6415d752184edc1
«Sergeityumen72» <>
2023-08-29 16:02:11 +0500

    dz

---
+++ Task16/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1ое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2ое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 / num2 == num2)
+// {
+//     Console.WriteLine("Верно");
+// }
+// else
+// {
+//     Console.WriteLine("Не верно");
+// }
+bool result = MultTwoNum(num1, num2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool MultTwoNum(int numb1, int numb2)
+{
+    return numb1 / numb2 == numb2 || numb2 / numb1 == numb1;
+}

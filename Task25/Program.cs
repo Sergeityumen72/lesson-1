ebed0fcf89b4fc887377e4050cce4d26aa00490d
«Sergeityumen72» <>
2023-09-05 12:52:50 +0500

    sept

---
+++ Task25/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+static int Power(int a, int b)
+{
+    int result = 1;
+
+    for (int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = Power(a, b);
+
+Console.WriteLine($"{a} в степени {b} равно {result}");
+Console.ReadKey();
